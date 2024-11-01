2c4d4b42a22d2f7f2fa8b90a30e0e0d864e797f3
grizeus <zaytsewww@gmail.com>
2024-11-01 13:22:20 +0200

    initial post-fork commit

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './components/App/App.jsx'
 import "modern-normalize"
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <App />
   </StrictMode>,
