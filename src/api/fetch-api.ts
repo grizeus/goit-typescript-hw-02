f17c8c1be35cf23e539f5d1ef70bc1fe7ee47686
grizeus <zaytsewww@gmail.com>
2024-11-02 14:37:18 +0200

    .

--- src/api/fetch-api.ts
+++ src/api/fetch-api.ts
@@ -1,29 +1,31 @@
-import axios from "axios";
+import axios, { AxiosPromise } from "axios";
 
 import type { Response } from "../types";
 
-const instance = axios.create(
-  {
-    baseURL: "https://api.unsplash.com/search/photos",
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": "Client-ID 7QaC31QWQatTvaiGVBFYsWoswGiVg-edIcET13XO0oA",
-      "Accept": "application/json",
-      "Accept-Version": "v1",
-    },
-  }
-);
+const instance = axios.create({
+  baseURL: "https://api.unsplash.com/search/photos",
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: "Client-ID 7QaC31QWQatTvaiGVBFYsWoswGiVg-edIcET13XO0oA",
+    Accept: "application/json",
+    "Accept-Version": "v1",
+  },
+});
 
-export const fetchImages = async (query: string, page = 1, perPage = 12) : Promise<Response> => {
+export const fetchImages = async (
+  query: string,
+  page = 1,
+  perPage = 12
+): Promise<Response> => {
   const CONFIG = {
     params: {
       query: query,
       page: page,
       per_page: perPage,
       content_filter: "low",
-    }
+    },
   };
-  const res = await instance.get<Response>("/", CONFIG);
+  const res: any = await instance.get<Response>("/", CONFIG);
 
-  return res.data;
+  return res.data as Promise<Response>;
 };
